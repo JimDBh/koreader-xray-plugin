e0501e5f386dbb920e3e1c5d7e3f8fd87a78f8a0
Jimmy Pautz <jpautz@watermarkinsights.com>
2026-06-11 07:30:30 -0500

    updatd test

--- spec/mocks/xray/series/the_wheel_of_time.lua
+++ spec/mocks/xray/series/the_wheel_of_time.lua
@@ -1,21 +1,21 @@
 -- X-Ray Series Cache v6.0
 return {
+  cache_version = "6.0",
   books = {
     [1] = {
       locations = {
         [1] = {
-          description = "Capital city",
           name = "Luthadel",
+          description = "Capital city",
         },
       },
       characters = {
         [1] = {
-          description = "Survivor of Hathsin",
           name = "Kelsier",
+          description = "Survivor of Hathsin",
         },
       },
     },
   },
-  cached_at = 1781180664,
-  cache_version = "6.0",
+  cached_at = 1781180944,
 }
